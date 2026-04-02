2ec19edc1d76a8b61388b209bdbdc48e92b70cbb
Mckayla Bodenstein <mckaylabekker@gmail.com>
2026-04-02 13:14:05 +0200

    Fixed correct Firebase CDN config

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsFMGqpd0ykkAA06gNTVtIBbioZVMIkmu",
-  authDomain: "urbanthreadsstore-b1b16.firebaseapp.com",
-  projectId: "urbanthreadsstore-b1b16",
-  storageBucket: "urbanthreadsstore-b1b16.firebasestorage.app",
-  messagingSenderId: "431050769185",
-  appId: "1:431050769185:web:712b7020fb84b4573e7460",
-  measurementId: "G-Z82PPSGXBv"
-};
+    apiKey: "AIzaSyBsFMGqpd0ykkAA06gNTVtIBbioZVMIkmU",
+    authDomain: "urbanthreadsstore-b1b16.firebaseapp.com",
+    projectId: "urbanthreadsstore-b1b16",
+    storageBucket: "urbanthreadsstore-b1b16.firebasestorage.app",
+    messagingSenderId: "431050769185",
+    appId: "1:431050769185:web:712b7020fb84b4573e7460",
+    measurementId: "G-Z82PPSGXBV"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
